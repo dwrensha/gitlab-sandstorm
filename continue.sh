ad8fa9443630daad300607ec28b31f38683b4f90
David Renshaw <dwrenshaw@gmail.com>
2015-01-17 13:11:23 -0500

    remove dependency on foreman

--- continue.sh
+++ continue.sh
@@ -14,7 +14,8 @@ cd gitlab
 
 export SSH_CONNECTION=12345
 
-RAILS_ENV=production bundle exec foreman start
-#bundle exec rails server -p 10000 -e production
+#RAILS_ENV=production bundle exec foreman start
+RAILS_ENV=production bundle exec sidekiq -q post_receive -q default 2>&1 | awk '{print "sidekiq: " $0}' &
+bundle exec rails server -p 10000 -e production
 
 
--- start.sh
+++ start.sh
@@ -33,8 +33,9 @@ export SSH_CONNECTION=12345
 
 cd gitlab
 
-RAILS_ENV=production bundle exec foreman start
-#bundle exec rails server -p 10000 -e production
+#RAILS_ENV=production bundle exec foreman start
+RAILS_ENV=production bundle exec sidekiq -q post_receive -q default 2>&1 | awk '{print "sidekiq: " $0}' &
+bundle exec rails server -p 10000 -e production
 
 
 
