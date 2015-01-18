dc6cb3ab9f597fc3a5f5ffec999bf9093596cfc2
David Renshaw <dwrenshaw@gmail.com>
2015-01-17 21:00:41 -0500

    don't print the ruby version on startup

--- continue.sh
+++ continue.sh
@@ -2,9 +2,6 @@ export RBENV_ROOT=/usr/local/share/rbenv
 export PATH="$RBENV_ROOT/bin:$PATH"
 eval "$(rbenv init -)"
 
-ruby --version
-which ruby
-
 rm -f /var/tmp/pids/*
 
 redis-server /etc/redis.conf &
--- start.sh
+++ start.sh
@@ -21,9 +21,6 @@ export RBENV_ROOT=/usr/local/share/rbenv
 export PATH="$RBENV_ROOT/bin:$PATH"
 eval "$(rbenv init -)"
 
-ruby --version
-which ruby
-
 redis-server /etc/redis.conf &
 echo "started redis-server: " $?
 
