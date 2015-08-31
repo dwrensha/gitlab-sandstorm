4c07d58cc59f3f2c37a8b81773012555fe1a61ee
David Renshaw <david@sandstorm.io>
2015-08-31 15:58:19 -0400

    start the archive_repo worker too

--- continue.sh
+++ continue.sh
@@ -10,7 +10,7 @@ export SSH_CONNECTION=12345 # gitlab-shell wants this variable to be set. Any va
 cd gitlab
 
 export GEM_HOME=/gitlab/.bundle/ruby/2.1.0
-RUBYOPT=-r/gitlab/.bundle/bundler/setup RAILS_ENV=production ./.bundle/ruby/2.1.0/bin/sidekiq -q post_receive -q default 2>&1 | awk '{print "sidekiq: " $0}' &
+RUBYOPT=-r/gitlab/.bundle/bundler/setup RAILS_ENV=production ./.bundle/ruby/2.1.0/bin/sidekiq -q post_receive -q default -q archive_repo 2>&1 | awk '{print "sidekiq: " $0}' &
 ./bin/rails server -p 10000 -e production
 
 
