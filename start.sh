0b6a81803fd24aeaaf2458c4774e03dc94a75d7c
David Renshaw <david@sandstorm.io>
2015-09-01 16:52:34 -0400

    Call continue.sh from start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-export SECRET_KEY_BASE=`base64 /dev/urandom | head -c 30`
-
 set -x
 
 mkdir -p /var/redis
@@ -14,18 +12,9 @@ mkdir -p /var/uploads
 
 mkdir -p /var/sqlite3
 
-redis-server /etc/redis.conf &
-echo "started redis-server: " $?
-
 cp initdb.sqlite3 /var/sqlite3/db.sqlite3
 
-export SSH_CONNECTION=12345 # gitlab-shell wants this variable to be set. Any value will do.
-
-cd gitlab
-export GEM_HOME=/gitlab/.bundle/ruby/2.1.0
-RUBYOPT=-r/gitlab/.bundle/bundler/setup RAILS_ENV=production ./.bundle/ruby/2.1.0/bin/sidekiq -q post_receive -q default -q archive_repo 2>&1 | awk '{print "sidekiq: " $0}' &
-./bin/rails server -p 10000 -e production
-
+source continue.sh
 
 
 
